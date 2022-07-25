b5c1fdecd8bd13b2ad20359f4e3bfc5d9eb60000
priyanka Chauhan <104743952+priyanka15102001@users.noreply.github.com>
2022-07-25 13:00:58 +0530

    Delete UserRepository.java

--- UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.wipro.springboot.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.wipro.springboot.Entity.User;
-
-public interface UserRepository extends JpaRepository<Admin, Integer> {
-	 
-	}
